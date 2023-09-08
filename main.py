29fead68bab3b606f60b604add81f2946ab058ca
Zbc <137158632+sissi-s-artlife@users.noreply.github.com>
2023-09-08 19:07:57 +0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import nltk
+import ssl
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+
 # Download VADER lexicon if not already downloaded
-nltk.download("vader_lexicon")
+nltk.download("vader_lexicon", quiet=True)
 
 # Initialize the VADER sentiment analyzer
 sia = SentimentIntensityAnalyzer()
